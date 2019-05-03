624f55c06b68edece8e3b93f0cb90e5c34f947cc
Roger Chao <rogman@gmail.com>
2019-05-02 21:50:23 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+//--------------------------------------------------------------------
+// Modules
+module "vpc" {
+  source  = "app.terraform.io/rogercorp/vpc/aws"
+  version = "1.64.0"
+
+  azs = "us-west-2b"
+  cidr = "10.0.0.0/16"
+}
