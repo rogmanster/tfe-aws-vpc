0eb12f698e9525022b9f2c948b3a80d5a305b266
Roger Chao <rogman@gmail.com>
2019-05-06 17:06:33 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
 output "vpc_id" {
   value = "${module.vpc.vpc_id}"
 }
+
+output "public_subnets" {
+  value = "${module.public_subnets}"
+
+output "private_subnets" {
+  value = "${module.private_subnets}"
