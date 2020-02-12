ddaebd510d9c43cfc43860240ff25a6ff1b86c70
Roger Chao <rogman@gmail.com>
2020-02-12 01:10:08 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,8 @@ module "vpc" {
   source  = "app.terraform.io/rogercorp/vpc/aws"
   version = "1.64.0"
   
-  #~Note: change default aws region to change azs~#
-  azs = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  #azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  az = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.139.1.0/24", "10.139.2.0/24", "10.139.3.0/24"]
   public_subnets  = ["10.139.101.0/24", "10.139.102.0/24", "10.139.103.0/24"]
   cidr            = "10.139.0.0/16"
