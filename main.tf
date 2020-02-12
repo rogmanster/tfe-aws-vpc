67845b08b1b9ec6d31a9b1552a8d9d2705142554
Roger Chao <rogman@gmail.com>
2020-02-12 00:37:10 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ module "vpc" {
   source  = "app.terraform.io/rogercorp/vpc/aws"
   version = "1.64.0"
 
-  azs = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.139.4.0/24", "10.139.5.0/24", "10.139.6.0/24"]
+  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  #azs = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.139.1.0/24", "10.139.2.0/24", "10.139.3.0/24"]
   public_subnets  = ["10.139.101.0/24", "10.139.102.0/24", "10.139.103.0/24"]
   cidr = "10.139.0.0/16"
 }
