594145525178748ea80a52318cc0180dcfd41ac4
Roger Chao <rogman@gmail.com>
2020-02-12 18:58:21 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.140.1.0/24", "10.140.2.0/24", "10.140.3.0/24"]
   public_subnets  = ["10.140.101.0/24", "10.140.102.0/24", "10.140.103.0/24"]
+  #cidr            = "10.140.0.0/16" #~toggle cidr block to invoke change for run triggers~#
   cidr            = "10.0.0.0/8"
 }
   
