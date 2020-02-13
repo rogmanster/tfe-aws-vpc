8da4e48d3ea06f1708de6fa2ff9d843e888712e7
Roger Chao <rogman@gmail.com>
2020-02-12 19:02:21 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ module "vpc" {
   #~toggle below blocks to invoke run trigger change~#
   private_subnets = ["10.139.1.0/24", "10.139.2.0/24", "10.139.3.0/24"]
   public_subnets  = ["10.139.101.0/24", "10.139.102.0/24", "10.139.103.0/24"]
-  cidr            = "10.139.0.0/8"
+  cidr            = "10.139.0.0/16"
 }
   
