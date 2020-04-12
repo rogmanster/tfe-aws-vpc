c672103abcc1efde6a3bc41f9c32afc9b837c631
Roger Chao <rogman@gmail.com>
2020-04-11 22:55:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source  = "app.terraform.io/rogercorp/vpc/aws"
   version = "2.24.0"
   
-  name            = "${var.name}-${var.aws_region}"  
+  name            = var.name
   #~toggle below secondary cidr blocks to invoke run trigger change~#
   #secondary_cidr_blocks = [ "10.139.0.0/16" ]
-  private_subnets = ["10.140.1.0/24", "10.140.2.0/24", "10.140.3.0/24"]
-  public_subnets  = ["10.140.101.0/24", "10.140.102.0/24", "10.140.103.0/24"]
-  cidr            = "10.140.0.0/16"
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+  cidr            = var.cidr_block
 }
   
