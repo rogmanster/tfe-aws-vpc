ba9c4bae648c2b0e28bc0c898586ff0d0714594d
Roger Chao <rogman@gmail.com>
2020-04-12 00:50:31 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+data "aws_availability_zones" "available" {
+}
+
 module "vpc" {
   source  = "app.terraform.io/rogercorp/vpc/aws"
   version = "2.24.0"
@@ -5,6 +8,7 @@ module "vpc" {
   name            = var.name
   #~toggle below secondary cidr blocks to invoke run trigger change~#
   #secondary_cidr_blocks = [ "10.139.0.0/16" ]
+  azs              = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1], data.aws_availability_zones.available.names[2]]
   private_subnets = ["10.139.1.0/24", "10.139.2.0/24", "10.139.3.0/24"]
   public_subnets  = ["10.139.101.0/24", "10.139.102.0/24", "10.139.103.0/24"]
   cidr            = "10.139.0.0/16"
