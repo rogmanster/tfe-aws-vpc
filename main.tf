fe163fc66c07d4b8ab63608690f27eea1c218258
Roger Chao <rogman@gmail.com>
2020-04-12 00:33:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ module "vpc" {
   name            = var.name
   #~toggle below secondary cidr blocks to invoke run trigger change~#
   #secondary_cidr_blocks = [ "10.139.0.0/16" ]
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
-  cidr            = var.cidr
+  private_subnets = ["10.139.1.0/24", "10.139.2.0/24", "10.139.3.0/24"]
+  public_subnets  = ["10.139.101.0/24", "10.139.102.0/24", "10.139.103.0/24"]
+  cidr            = "10.139.0.0/16"
 }
   
